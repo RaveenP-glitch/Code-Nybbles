cb63fe43a78056524342c92e9c0705ae0c850ccb
Raveen Panditha <raveenpanditha9@gmail.com>
2022-12-10 22:09:33 +0530

    New React app

---
+++ react-guide/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Let's get started</h2>
+     <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
